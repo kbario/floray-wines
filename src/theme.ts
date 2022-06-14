0ff48372a0223f9dd8a178c34ef8413a6a282610
kbario <kylebario1@gmail.com>
2022-06-14 19:27:44 +0800

    theming updated

--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,23 @@ import { blue } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
-    primary: blue,
+    primary: {
+      main: "#0F232F",
+    },
+    secondary: {
+      main: "#F8FAFC",
+    },
   },
 });
 
 export default theme;
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    bg?: Palette["primary"];
+    gun?: Palette["primary"];
+  }
+  interface PaletteOptions {
+    bg?: PaletteOptions["primary"];
+  }
+}
