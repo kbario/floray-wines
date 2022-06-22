b477715d3e0d9235645b5c160bbc10100dae8845
kbario <kylebario1@gmail.com>
2022-06-22 21:13:28 +0800

    deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/floray-wines/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        // nested: resolve(__dirname, '')
       },
     },
   },
